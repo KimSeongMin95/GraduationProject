fbe5559ee2b4689076140eaf25a5fe80acec7af5
KimSeongMin95 <kimseongmin950310@gmail.com>
2020-02-08 13:24:47 +0900

    server

---
+++ Game/ServerProjects/IocpServer/main.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <stdio.h>
+#include <tchar.h>
+
+#include "MainServer.h"
+
+int main()
+{
+	MainServer iocp_server;
+
+	if (iocp_server.Initialize())
+		iocp_server.StartServer();
+
+	return 0;
+}
